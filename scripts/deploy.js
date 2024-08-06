38f883fc88ea966d1b28f0aeed191db24298eb95
sanechka4923 <22ipf.o.bochkarov@std.npu.edu.ua>
2024-08-06 17:45:47 +0000

    Initial commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik"]);
-  await contract.waitForDeployment();
-  console.log(`Swisstronik contract deployed to ${contract.target}`);
-}
 
+  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik!!"]);
+
+  await contract.waitForDeployment();
+
+  console.log(`This is your deployed contract address ${contract.target}`);
+}
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
